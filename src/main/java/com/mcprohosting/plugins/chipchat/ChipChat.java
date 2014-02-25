6f4836e3e683a92dd898d39ff7cf40e5eb540bb1
Evan Lindsay <evanlindsay@live.com>
2014-02-24 21:22:06 -0800

    Command and configuration api

--- src/main/java/com/mcprohosting/plugins/chipchat/ChipChat.java
+++ src/main/java/com/mcprohosting/plugins/chipchat/ChipChat.java
@@ -1,19 +1,38 @@
 package com.mcprohosting.plugins.chipchat;
 
+import com.mcprohosting.plugins.chipchat.api.command.CommandHandler;
+import com.mcprohosting.plugins.chipchat.commands.Channel;
+import com.mcprohosting.plugins.chipchat.configuration.Config;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ChipChat extends JavaPlugin {
 
     private static ChipChat plugin;
+    private static Config config;
+    private static CommandHandler commandHandler;
 
     public void onEnable() {
         plugin = this;
+        config = new Config(this);
+        commandHandler = new CommandHandler(this);
+
+        registerCommands();
     }
 
     public void onDisable() {}
 
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return commandHandler.handleCommand(sender, label, command, args);
+    }
+
     public static ChipChat getPlugin() {
         return plugin;
+    }
+
+    private void registerCommands() {
+        commandHandler.registerCommands(new Channel(this));
     }
 
 }
---
+++ src/main/java/com/mcprohosting/plugins/chipchat/configuration/Config.java
@@ -0,0 +1,30 @@
+package com.mcprohosting.plugins.chipchat.configuration;
+
+import com.mcprohosting.plugins.chipchat.api.configuration.ConfigModel;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.plugin.Plugin;
+
+import java.io.File;
+
+public class Config extends ConfigModel {
+
+    private static Config config;
+
+    public Config(Plugin plugin) {
+        CONFIG_FILE = new File(plugin.getDataFolder(), "config.yml");
+        CONFIG_HEADER = "ChipChat Configuration File";
+
+        try {
+            this.init();
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+
+        config = this;
+    }
+
+    public static Config getConfig() {
+        return config;
+    }
+
+}
